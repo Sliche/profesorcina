3db0ae0c99fe23e0ba489c8da7e4425be5da8e3d
Sliche <sm.boriss@hotmail.com>
2016-03-28 02:46:18 +0200

    a

--- project/resources/views/app.blade.php
+++ project/resources/views/app.blade.php
@@ -2,60 +2,25 @@
 <html>
 <head>
 
+	{{--<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">--}}
+	{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>--}}
+	{{--<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>--}}
 
- 
- </head>
+
+	<link rel="stylesheet" href="/twbs/bootstrap/dist/css/bootstrap.min.css">
+	<script src="/components/jquery/jquery.min.js"></script>
+	<script src="/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
+
+
+</head>
 
 <body>
 
-
-
-<nav class="navbar navbar-default">
-
-  <div class="container-fluid">
-
-
-    <div class="navbar-header">
-      <!-- <a class="navbar-brand" href="#">WebSiteName</a> -->
-    </div>
-
-
-
-    <ul class="nav navbar-nav">
-      <li><a href="#"><img src="logo.pnglolol"></a></li>
-    </ul>
-
-	<!-- <ul class="nav navbar-nav float-right"> -->
-	  <ul class="nav navbar-nav navbar-right">
-
-      <li><a href="#">Pocetna</a></li>
-      <li><a href="#">Ustanove</a></li>
-      <li><a href="#">Kako radi APP</a></li>
-      <li><a href="#">Kontakt</a></li>
-    </ul>
-
-
-
-
-
-  </div>
-
-
-</nav>
-
-
-
-
-
-
-
-
-
-
-
-	<div class="container">	
-		@yield('content')
-	</div>
+<div class="container">
+	@yield('content')
+</div>
 
 
 
