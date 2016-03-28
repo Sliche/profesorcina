18380cc3254fb33ab62d40cbaf9d43a1cb6e06d4
Sliche <sm.boriss@hotmail.com>
2016-03-28 02:33:16 +0200

    changes

--- project/resources/views/app.blade.php
+++ project/resources/views/app.blade.php
@@ -2,9 +2,17 @@
 <html>
 <head>
 
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	{{--<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">--}}
+	{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>--}}
+	{{--<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>--}}
+
+
+	<link rel="stylesheet" href="/twbs/bootstrap/dist/css/bootstrap.min.css">
+	<script src="/components/jquery/jquery.min.js"></script>
+	<script src="/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
+
  
  </head>
 
