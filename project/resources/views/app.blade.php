5196c2798bc157335be853a4d73d3d5ad68ed1f2
Mihajlo <mihajlomiklos@gmail.com>
2016-03-28 02:34:36 +0200

    meni

--- project/resources/views/app.blade.php
+++ project/resources/views/app.blade.php
@@ -3,12 +3,55 @@
 <head>
 
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min`.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
  
  </head>
 
 <body>
+
+
+
+<nav class="navbar navbar-default">
+
+  <div class="container-fluid">
+
+
+    <div class="navbar-header">
+      <!-- <a class="navbar-brand" href="#">WebSiteName</a> -->
+    </div>
+
+
+
+    <ul class="nav navbar-nav">
+      <li><a href="#"><img src="logo.pnglolol"></a></li>
+    </ul>
+
+	<ul class="nav navbar-nav float-right">
+      <li><a href="#">Pocetna</a></li>
+      <li><a href="#">Ustanove</a></li>
+      <li><a href="#">Kako radi APP</a></li>
+      <li><a href="#">Kontakt</a></li>
+    </ul>
+
+
+
+
+
+  </div>
+
+
+</nav>
+
+
+
+
+
+
+
+
+
+
 
 	<div class="container">	
 		@yield('content')
--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 
-<h1> aoijsfoiajs</h1>
 
 
 
